0d7fa43bca7b8802831907c3f87731af766084ea
mora5925 <wahba190@gmail.com>
2020-11-19 09:06:25 -0500

    src folder

--- employee-directory/src/Components/DataTable.js
+++ employee-directory/src/Components/DataTable.js
@@ -1,13 +1,42 @@
 import React, { Component } from "react";
 import DataBody from "./DataBody";
-import SearchBox from "./SearchBox";
+import Nav from "./Nav";
 import API from "../Utils/API";
 
 class DataTable extends Component {
-  state = {
-    result: [],
-    alphabetical: "az",
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      alphabetical: "az",
+      result: [],
+      resultSort: [],
+      search: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ search: event.target.value });
+    const search = event.target.value;
+    const foundEmployees = this.state.result.filter((name) => {
+      let values = name.name.first.toLowerCase();
+      return values.indexOf(search.toLowerCase()) !== -1;
+    });
+
+    this.setState({ resultSort: foundEmployees });
   };
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
 
   componentDidMount() {
     this.userInfo();
@@ -19,34 +48,48 @@ class DataTable extends Component {
       .catch((err) => console.log(err));
   };
 
+  resultsOrSearched = () => {
+    if (this.state.search !== "") {
+      return (
+        <DataBody sortByName={this.sortByName} result={this.state.resultSort} />
+      );
+    } else {
+      return (
+        <DataBody sortByName={this.sortByName} result={this.state.result} />
+      );
+    }
+  };
+
   render() {
     let sortedUsers;
 
     if (this.state.alphabetical === "az") {
-      console.log("sort");
       sortedUsers = this.state.result.sort((a, b) =>
         a.name.last > b.name.last ? 1 : -1
       );
     } else {
+      
       sortedUsers = this.state.result.sort((a, b) =>
         a.name.last < b.name.last ? 1 : -1
       );
     }
 
     return (
-      <table className="table table-hover table-dark">
-        <SearchBox />
-        <thead>
-          <tr>
+      <div>
+        <Nav handleInputChange={this.handleInputChange} />
+        <table className="table table-hover table-dark">
+          <thead>
+            <tr>
             <th scope="col">Image</th>
-            <th scope="col">Name</th>
-            <th scope="col">Phone</th>
-            <th scope="col">Email</th>
-            <th scope="col">DOB</th>
-          </tr>
-        </thead>
-        <DataBody result={this.state.result} />
-      </table>
+              <th scope="col">Name</th>
+              <th scope="col">Phone</th>
+              <th scope="col">Email</th>
+              <th scope="col">DOB</th>
+            </tr>
+          </thead>
+          {this.resultsOrSearched()};
+        </table>
+      </div>
     );
   }
 }
